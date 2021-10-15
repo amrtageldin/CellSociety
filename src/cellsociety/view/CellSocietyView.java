5486de499c6ad79c93ee124bb7578694b77b9a9d
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-15 12:26:48 -0400

    Added Upload Simulation File and Initial Grid File Buttons with a Label

---
+++ src/cellsociety/view/CellSocietyView.java
@@ -0,0 +1,71 @@
+package cellsociety.view;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Objects;
+
+/**
+ * @author Evelyn Cupil-Garcia
+ * <p>
+ * Class that sets up the display for all Cell Society Game types.
+ */
+public class CellSocietyView {
+
+  private final FactoryComponents myFactoryComponents;
+  private final Stage myStage;
+
+
+  private static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.view.resources.";
+  private static final String DEFAULT_STYLESHEET =
+      "/" + DEFAULT_RESOURCE_PACKAGE.replace(".", "/") + "Default.css";
+
+  /**
+   * Constructor for the CellSocietyView class that initializes FactoryComponents and retrieves
+   * Stage.
+   *
+   * @param language What language property will be used (English or Spanish).
+   * @param stage    Stage from Main class to call upon files.
+   */
+  public CellSocietyView(String language, Stage stage) {
+    myFactoryComponents = new FactoryComponents(language);
+    myStage = stage;
+  }
+
+  /**
+   * Method that sets up the Display.
+   *
+   * @return scene that contains the file buttons for the user to choose a game.
+   */
+  public Scene setupDisplay() {
+    VBox root = new VBox();
+    Node displayLabel = myFactoryComponents.makeLabel("DisplayLabel");
+    root.getChildren().addAll(displayLabel, setupGameModePanel());
+    Scene scene = new Scene(root);
+    scene.getStylesheets()
+        .add(Objects.requireNonNull(getClass().getResource(DEFAULT_STYLESHEET)).toExternalForm());
+    return scene;
+  }
+
+  private Node setupGameModePanel() {
+    HBox panel = new HBox();
+    Node simulationType = myFactoryComponents.makeButton("SimulationType",
+        e -> chooseFile(myStage));
+    Node initialGrid = myFactoryComponents.makeButton("InitialGrid", e -> chooseFile(myStage));
+    panel.getChildren().addAll(simulationType, initialGrid);
+    return panel;
+  }
+
+  private void chooseFile(Stage stage) {
+    FileChooser fileChooser = new FileChooser();
+    File selectedFile = fileChooser.showOpenDialog(stage);
+    // would need to send selectedFile to Controller.
+  }
+
+
+}
---
+++ src/cellsociety/view/FactoryComponents.java
@@ -0,0 +1,62 @@
+package cellsociety.view;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.util.ResourceBundle;
+
+/**
+ * Class that creates individual components for the View.
+ *
+ * @author Evelyn Cupil-Garcia
+ */
+public class FactoryComponents {
+
+  private final ResourceBundle myResources;
+
+  private static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.view.resources.";
+
+  /**
+   * Constructor for the FactoryComponents class that initializes it's resource bundle.
+   *
+   * @param language What language property will be used (English or Spanish).
+   */
+  public FactoryComponents(String language) {
+    myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + language);
+  }
+
+  /**
+   * Method that creates a Label.
+   *
+   * @param id Label identifier.
+   * @return Label with identifier.
+   */
+  public Label makeLabel(String id) {
+    Label label = new Label(myResources.getString(id));
+    return (Label) setId(id, label);
+  }
+
+  /**
+   * Method that creates a Button.
+   *
+   * @param label   Button identifier for id/text.
+   * @param handler ActionHandler when Button is pressed.
+   * @return Node that has id as label and the button itself.
+   */
+  public Node makeButton(String label, EventHandler<ActionEvent> handler) {
+    Button result = new Button();
+    result.setText(myResources.getString(label));
+    result.setOnAction(handler);
+    return setId(label, result);
+  }
+
+  private Node setId(String id, Node node) {
+    node.setId(id);
+    return node;
+  }
+
+
+}
