0407ad3b2c9425aaf6daff2dbcff16793183d741
Reya Magan <rm357@duke.edu>
2021-10-15 16:23:13 -0400

    reya and amr pair programming to set up grid

--- src/cellsociety/controller/GridFactory.java
+++ src/cellsociety/controller/GridFactory.java
@@ -1,8 +1,55 @@
 package cellsociety.controller;
 
+import cellsociety.model.Cells;
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GridFactory {
+    private Cells[][] myGrid;
 
     public void setUpGrid(String file){
-        System.out.println("reached");
+        try {
+            FileReader fileReader = new FileReader(file);
+            CSVReader csvReader = new CSVReader(fileReader);
+
+            initializeRowAndColumnCounts(csvReader);
+            initializeCells(csvReader);
+
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace(); //NEED TO COME BACK AND FIX!!!
+        }
+    }
+
+    private void initializeRowAndColumnCounts(CSVReader csvReader){
+        try {
+            String[] rowAndColumn = csvReader.readNext();
+            for(String c : rowAndColumn){
+                System.out.println(c);
+            }
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeCells(CSVReader csvReader){
+       try {
+           String nextCell[];
+           while ((nextCell = csvReader.readNext()) != null) {
+               for (String cell : nextCell) {
+                   System.out.print(cell);
+               }
+               System.out.println();
+           }
+       }
+       catch(Exception e){
+           e.printStackTrace(); //NEED TO CHANGE LATER
+       }
+
     }
 }
