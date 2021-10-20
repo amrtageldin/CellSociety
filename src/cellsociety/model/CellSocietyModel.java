0d3b51339543132ba7611f4b8f4e37bbd01bd920
Amr Tagel-Din <aht14@duke.edu>
2021-10-20 14:57:06 -0400

    Refactored CellSocietyModel to reduce redundancy

--- src/cellsociety/model/CellSocietyModel.java
+++ src/cellsociety/model/CellSocietyModel.java
@@ -1,6 +1,9 @@
 package cellsociety.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CellSocietyModel {
   public CellSocietyModel(){
 
@@ -13,6 +16,43 @@ public class CellSocietyModel {
 
   public void setNextState(Cells myCell, int row, int col, Cells[][] myGrid){
 
+  }
+
+  protected int quantityOfCellsOfGivenStateInCluster(int state, List<Cells> myRelevantCluster) {
+    int runningCountOfState = 0;
+    for (Cells x : myRelevantCluster){
+      if (x.getCurrentState() == state) {
+        runningCountOfState++;
+      }
+    }
+    return runningCountOfState;
+  }
+
+
+  protected List<Cells> generateNeighbors(int row, int col, Cells[][] myGrid) {
+    // square
+    int[] xChanges = new int[]{-1,0,1};
+    int[] yChanges = new int[]{-1,0,1};
+    List<Cells> myCells = new ArrayList<>();
+    for (int i : xChanges){
+      if (rowIsValid(row + i, myGrid)){
+        for (int j : yChanges){
+          if (colIsValid(col + j, myGrid) && !(i == 0 && j == 0)){
+            myCells.add(myGrid[row + i][ col + j]);
+          }
+        }
+
+      }
+    }
+    return myCells;
+  }
+
+  private boolean colIsValid(int col, Cells[][] myGrid) {
+    return col >=0 && col < myGrid[0].length;
+  }
+
+  private boolean rowIsValid(int row, Cells[][] myGrid) {
+    return row >=0 && row < myGrid.length;
   }
 
 
--- src/cellsociety/model/PercolationModel.java
+++ src/cellsociety/model/PercolationModel.java
@@ -1,9 +1,6 @@
 package cellsociety.model;
 
-import cellsociety.ruleStructure.CellSocietyRules;
-import cellsociety.ruleStructure.GameOfLifeRules;
 import cellsociety.ruleStructure.PercolationRules;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PercolationModel extends CellSocietyModel{
@@ -25,44 +22,8 @@ public class PercolationModel extends CellSocietyModel{
   @Override
   public void setNextState(Cells myCell, int row, int col, Cells[][] myGrid){
     List<Cells> myNeighbors = generateNeighbors(row,col, myGrid);
-    int quantityOfPercolatedCells = relevantClumpQuantityInDesiredState(PERCOLATED, myNeighbors);
+    int quantityOfPercolatedCells = quantityOfCellsOfGivenStateInCluster(PERCOLATED, myNeighbors);
     myCell.setMyNextState(myRules.generateNextState(quantityOfPercolatedCells, myCell.getCurrentState()));
-  }
-
-  private List<Cells> generateNeighbors(int row, int col, Cells[][] myGrid) {
-    // square
-    int[] xChanges = new int[]{-1,0,1};
-    int[] yChanges = new int[]{-1,0,1};
-    List<Cells> myCells = new ArrayList<>();
-    for (int i : xChanges){
-      if (rowIsValid(row + i, myGrid)){
-        for (int j : yChanges){
-          if (colIsValid(col + j, myGrid) && !(i == 0 && j == 0)){
-            myCells.add(myGrid[row + i][ col + j]);
-          }
-        }
-
-      }
-    }
-    return myCells;
-  }
-
-  private boolean colIsValid(int col, Cells[][] myGrid) {
-    return col >=0 && col < myGrid[0].length;
-  }
-
-  private boolean rowIsValid(int row, Cells[][] myGrid) {
-    return row >=0 && row < myGrid.length;
-  }
-
-  private int relevantClumpQuantityInDesiredState(int state, List<Cells> myRelevantCluster) {
-    int runningCountOfState = 0;
-    for (Cells x : myRelevantCluster){
-      if (x.getCurrentState() == state) {
-        runningCountOfState++;
-      }
-    }
-    return runningCountOfState;
   }
 
 }
