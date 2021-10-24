cc2ed90cfc59fed6d5be87806c9f08c1095cbf70
Amr Tagel-Din <aht14@duke.edu>
2021-10-24 14:58:26 -0400

    Make fireRules probability based

--- src/cellsociety/ruleStructure/FireRules.java
+++ src/cellsociety/ruleStructure/FireRules.java
@@ -1,8 +1,36 @@
 package cellsociety.ruleStructure;
 
+import java.lang.reflect.Method;
+
 public class FireRules extends CellSocietyRules {
+  private String probPercent;
+
   public FireRules(){
     super();
+  }
+
+  @Override
+  protected void initializeMyRules(){
+    translationBundle = initializeBundle(ruleResourceBundleBase, "TranslationRules");
+
+    for (String eachKey : ruleBundle.keySet()){
+      String ruleString = ruleBundle.getString(eachKey);
+      String[]ruleSet = ruleString.split(" ");
+
+      try{
+        Method method = this.getClass().getDeclaredMethod(ruleSet[1]);
+        method.invoke(this);
+      }
+      catch (Exception e){
+        e.printStackTrace();
+      }
+      ruleSet[1] = probPercent;
+      runThroughRules(ruleSet);
+    }
+  }
+
+  private void probabilityCheck(){
+    probPercent = "30"; // changeLater
   }
 
   protected void prepBundles() {
