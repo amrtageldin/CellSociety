bdaf33fbb8a47d65ab506114cf4a3957bc99eae1
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-25 21:20:15 -0400

    Added all game colors

--- src/cellsociety/view/GridView.java
+++ src/cellsociety/view/GridView.java
@@ -2,7 +2,9 @@ package cellsociety.view;
 
 import cellsociety.controller.CellSocietyController;
 import cellsociety.model.Cells;
+import java.util.ArrayList;
 import java.util.List;;
+import java.util.Map;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -24,13 +26,39 @@ public class GridView {
   private static final int GAP = 1;
   private static final int SCREEN_WIDTH = 1500;
   private static final int SCREEN_HEIGHT = 600;
+  private static final String GAME_OF_LIFE = "GameOfLife";
+  private static final String FIRE = "Fire";
+  private static final String PERCOLATION = "Percolation";
+  private static final String SCHELLING_SEGREGATION = "SchellingSegregation";
 
-  private final List<Color> STATE_COLORS = List.of(
+  private final List<Color> LIFE_STATE_COLORS = List.of(
       Color.BLACK,      // dead cell color
-      Color.WHITE,       // alive cell color
-      Color.BLUE,          // water cell color
-      Color.GRAY
+      Color.WHITE       // alive cell color
   );
+
+  private final List<Color> FIRE_STATE_COLORS = List.of(
+      Color.RED, // fire color
+      Color.GREEN, // tree color
+      Color.GREEN, // tree color
+      Color.LIGHTGREY // empty
+  );
+
+  private final List<Color> PERCOLATE_STATE_COLORS = List.of(
+      Color.BLUE, // percolated color
+      Color.WHITE, // open color
+      Color.WHITE, // open color
+      Color.BLACK // closed color
+  );
+
+  private final List<Color> SS_STATE_COLORS = List.of(
+    Color.WHITE, // empty color
+    Color.GRAY, // filler color
+    Color.PURPLE, // filler color
+    Color.ORANGE // group b color
+  );
+
+
+  private List<Color> stateColors;
 
   /**
    * Constructor that initializes the Grid.
@@ -40,6 +68,13 @@ public class GridView {
   public GridView(CellSocietyController controller) {
     myGrid = controller.getMyGrid();
     myPaneNodes = new Rectangle[myGrid.length][myGrid[0].length];
+    stateColors = initializeGameMap(controller.getMyGameType());
+  }
+
+  private List<Color> initializeGameMap(String gameType) {
+    Map<String, List<Color>> map = Map.of(GAME_OF_LIFE, LIFE_STATE_COLORS, FIRE, FIRE_STATE_COLORS,
+        PERCOLATION, PERCOLATE_STATE_COLORS, SCHELLING_SEGREGATION, SS_STATE_COLORS);
+    return map.get(gameType);
   }
 
   /**
@@ -61,7 +96,7 @@ public class GridView {
     for (int i = 0; i < myGrid.length; i++) {
       for (int j = 0; j < myGrid[0].length; j++) {
         int currState = myGrid[i][j].getCurrentState();
-        Rectangle cell = drawCell(STATE_COLORS.get(currState));
+        Rectangle cell = drawCell(stateColors.get(currState));
         setCellClickAction(cell, i, j);
         myPaneNodes[i][j] = cell;
         pane.add(myPaneNodes[i][j], j, i);
@@ -73,7 +108,7 @@ public class GridView {
     EventHandler<MouseEvent> event = event1 -> {
       int setState = (myGrid[i][j].getCurrentState()==0) ? 1:0;
       myGrid[i][j].setCurrentState(setState);
-      Rectangle newCell = drawCell(STATE_COLORS.get(setState));
+      Rectangle newCell = drawCell(stateColors.get(setState));
       myPaneNodes[i][j] = newCell;
       pane.add(myPaneNodes[i][j], j, i);
     };
