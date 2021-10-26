373b014467c76679d31d644b8e6242e2f26990f8
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-26 19:44:16 -0400

    Fixed CellSocietyView to work locally, you can change values in Resource bundle to click in the correct area for a specific laptop

--- test/cellsociety/view/CellSocietyViewTest.java
+++ test/cellsociety/view/CellSocietyViewTest.java
@@ -3,11 +3,14 @@ package cellsociety.view;
 import cellsociety.controller.CellSocietyController;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.ResourceBundle;
 import javafx.scene.control.Button;
+import javafx.scene.control.Cell;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -18,22 +21,29 @@ import javafx.stage.Window;
 import org.junit.jupiter.api.Test;
 //import org.mockito.ArgumentMatchers;
 //import org.mockito.Mockito;
+import org.mockito.Mockito;
 import util.DukeApplicationTest;
 
 /**
  * @author Evelyn Cupil-Garcia
  * <p>
- * Class that does testing on the CellSocietyView class.
+ * Class that does testing on the CellSocietyView class. Note that we need to update testing so
+ * that there is mocking for FileChooser, currently I click on the screen.
  */
 public class CellSocietyViewTest extends DukeApplicationTest {
 
   public static final String TITLE = "Cell Society";
   public static final String LANGUAGE = "English";
-  public static final int FILE_X = 200;
-  public static final int SIM_Y = 220;
-  public static final int GRID_Y = 200;
-  public static final int OK_X = 480;
-  public static final int OK_Y = 330;
+  private static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.view.resources.";
+  private final ResourceBundle myMagicValues = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "ViewTestValues");
+
+  public static final String lifeFolderX = "lifeFolderX";
+  public static final String lifeFolderY = "lifeFolderY";
+  public static final String gridY = "gridY";
+  public static final String simY = "simY";
+  public static final String okX = "okX";
+  public static final String okY = "okY";
+  
   private CellSocietyView display;
   private Button mySimulation;
   private Button myInitialGrid;
@@ -68,12 +78,15 @@ public class CellSocietyViewTest extends DukeApplicationTest {
   @Test
   public void initialGridAction() {
     File expectedFile = new File("data/game_of_life/blinkers.csv");
-//    controller.loadFileType(expectedFile.toString());
-//    FileChooser fileChooser = Mockito.spy(FileChooser.class);
-//    Mockito.when(fileChooser.showSaveDialog(Mockito.any()));
-//    clickOn(myInitialGrid);
-//    clickOn(FILE_X, GRID_Y, MouseButton.PRIMARY);
-//    clickOn(OK_X, OK_Y, MouseButton.PRIMARY);
+    clickOn(myInitialGrid);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(lifeFolderY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(lifeFolderY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(gridY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(okX)),
+        Integer.parseInt(myMagicValues.getString(okY)), MouseButton.PRIMARY);
     File actualFile = display.getMyFile();
     try {
       assertEquals(expectedFile.getCanonicalPath(), actualFile.getCanonicalPath());
@@ -90,8 +103,14 @@ public class CellSocietyViewTest extends DukeApplicationTest {
   public void simulationTypeAction() {
     File expectedFile = new File("data/game_of_life/blinkers.sim");
     clickOn(mySimulation);
-    clickOn(FILE_X, SIM_Y, MouseButton.PRIMARY);
-    clickOn(OK_X, OK_Y, MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(lifeFolderY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(lifeFolderY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)), Integer.parseInt(myMagicValues.getString(simY))
+        , MouseButton.PRIMARY);
+    clickOn(Integer.parseInt(myMagicValues.getString(okX)),
+        Integer.parseInt(myMagicValues.getString(okY)), MouseButton.PRIMARY);
     File actualFile = display.getMyFile();
     try {
       assertEquals(expectedFile.getCanonicalPath(), actualFile.getCanonicalPath());
