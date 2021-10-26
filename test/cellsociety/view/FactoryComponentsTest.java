3052c5ccfd654d7d0e7a060c34ced3e894d0b75f
Luke Josephy <lrj11@duke.edu>
2021-10-26 15:01:09 -0400

    FactoryComponents testing over 80%

--- test/cellsociety/view/FactoryComponentsTest.java
+++ test/cellsociety/view/FactoryComponentsTest.java
@@ -2,6 +2,9 @@ package cellsociety.view;
 
 import java.util.ResourceBundle;
 import java.util.function.BooleanSupplier;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.paint.Color;
@@ -65,6 +68,26 @@ public class FactoryComponentsTest extends DukeApplicationTest {
     assertEquals(min, slider.getValue());
     assertEquals(increment, (int) slider.getBlockIncrement());
     assertEquals(label, slider.getId());
+  }
+
+  @Test
+  void makeDropDownMenu() {
+    String label = "Start/Pause";
+    String[] options = {"LightMode", "DarkMode"};
+    ComboBox dropdown = myFactoryComponents.makeDropDownMenu(label, options);
+
+    assertEquals(label, dropdown.getId());
+    assertEquals(label, dropdown.getPromptText());
+    assertEquals("Light Mode", dropdown.getItems().get(0));
+    assertEquals("Dark Mode", dropdown.getItems().get(1));
+  }
+
+  @Test
+  void makeButton() {
+    String label = "Start/Pause";
+    Node button = myFactoryComponents.makeButton(label, null);
+
+    assertEquals(label, button.getId());
   }
 }
 
