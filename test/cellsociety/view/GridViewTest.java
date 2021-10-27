5a9cf221bfab303a3ff170057303e7c845553202
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-26 22:03:18 -0400

    Fixed testing on GridView class

--- test/cellsociety/view/GridViewTest.java
+++ test/cellsociety/view/GridViewTest.java
@@ -1,14 +1,8 @@
 package cellsociety.view;
 
 import cellsociety.controller.CellSocietyController;
-import cellsociety.model.Cells;
-import java.util.List;
-import java.util.ResourceBundle;
-import javafx.scene.control.Cell;
-import javafx.scene.control.Label;
+import java.io.File;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 
@@ -16,35 +10,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import util.DukeApplicationTest;
 
 /**
- * @author Luke Josephy
+ * @author Luke Josephy, Evelyn Cupil-Garcia
  * <p>
  * Class that does testing on the GridView class.
  */
 
 public class GridViewTest extends DukeApplicationTest {
 
-  private Cells[][] myGrid;
   private GridView myGridView;
-  private CellSocietyController myCellSocietyController;
-  private Rectangle[][] myPaneNodes;
-  private GridPane pane;
-  private ResourceBundle myMagicValues;
-  private List<Color> stateColors;
-  private CellColors myCellColors;
-
-  public final String gap = "gap";
-  public final String screenWidth = "screenWidth";
-  public final String screenHeight = "screenHeight";
-  private static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.view.resources.";
 
   @Override
   public void start(Stage stage) {
-    myCellSocietyController = new CellSocietyController();
+    CellSocietyController myCellSocietyController = new CellSocietyController();
+    File csvFile = new File("data/game_of_life/blinkers.csv");
+    File simFile = new File("data/game_of_life/blinkers.sim");
+    myCellSocietyController.loadFileType(csvFile.toString());
+    myCellSocietyController.loadFileType(simFile.toString());
     myGridView = new GridView(myCellSocietyController);
-    myPaneNodes = new Rectangle[myGrid.length][myGrid[0].length];
-    myCellColors = new CellColors(myCellSocietyController.getMyGameType());
-    stateColors = myCellColors.getColorMap();
-    myMagicValues = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "MagicValues");
   }
 
   @Test
