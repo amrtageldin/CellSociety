a444f5bf84f28bcc3bc330bc5297b5c1173c2840
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-26 22:08:45 -0400

    Fixed testing for Frontend

--- test/cellsociety/view/CellSocietyViewTest.java
+++ test/cellsociety/view/CellSocietyViewTest.java
@@ -3,30 +3,16 @@ package cellsociety.view;
 import cellsociety.controller.CellSocietyController;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-
-import cellsociety.model.Cells;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.ResourceBundle;
 import javafx.scene.control.Button;
-import javafx.scene.control.Cell;
 import javafx.scene.control.ComboBox;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import org.junit.jupiter.api.Test;
-//import org.mockito.ArgumentMatchers;
-//import org.mockito.Mockito;
-import org.mockito.Mockito;
 import util.DukeApplicationTest;
 
 /**
@@ -39,20 +25,8 @@ public class CellSocietyViewTest extends DukeApplicationTest {
 
   public static final String TITLE = "Cell Society";
   public static final String LANGUAGE = "English";
-  private static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.view.resources.";
-  private final ResourceBundle myMagicValues = ResourceBundle.getBundle(
-      DEFAULT_RESOURCE_PACKAGE + "ViewTestValues");
-
-  public static final String lifeFolderX = "lifeFolderX";
-  public static final String lifeFolderY = "lifeFolderY";
-  public static final String gridY = "gridY";
-  public static final String simY = "simY";
-  public static final String okX = "okX";
-  public static final String okY = "okY";
 
   private CellSocietyView display;
-  private Button mySimulation;
-  private Button myInitialGrid;
   private Button myStart;
   private Button myPause;
   private CellSocietyController controller;
@@ -65,79 +39,26 @@ public class CellSocietyViewTest extends DukeApplicationTest {
   @Override
   public void start(Stage stage) {
     controller = new CellSocietyController();
+    File csvFile = new File("data/game_of_life/blinkers.csv");
+    File simFile = new File("data/game_of_life/blinkers.sim");
+    controller.loadFileType(csvFile.toString());
+    controller.loadFileType(simFile.toString());
     display = new CellSocietyView(controller, LANGUAGE, stage);
     stage.setScene(display.setupDisplay());
     stage.setTitle(TITLE);
     stage.setFullScreen(true);
     stage.show();
 
-    mySimulation = lookup("#SimulationType").query();
-    myInitialGrid = lookup("#InitialGrid").query();
     myStart = lookup("#Play").query();
     myPause = lookup("#Start/Pause").query();
   }
 
-  /**
-   * Unit test that checks when the upload simulation type file button is pressed, that the file
-   * explorer appears.
-   */
-  @Test
-  public void initialGridAction() {
-    File expectedFile = new File("data/game_of_life/blinkers.csv");
-    clickOn(myInitialGrid);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(lifeFolderY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(lifeFolderY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(gridY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(okX)),
-        Integer.parseInt(myMagicValues.getString(okY)), MouseButton.PRIMARY);
-    File actualFile = display.getMyFile();
-    try {
-      assertEquals(expectedFile.getCanonicalPath(), actualFile.getCanonicalPath());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Unit test that checks when the upload initial grid file button is pressed, that the file
-   * explorer appears.
-   */
-  @Test
-  public void simulationTypeAction() {
-    File expectedFile = new File("data/game_of_life/blinkers.sim");
-    clickOn(mySimulation);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(lifeFolderY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(lifeFolderY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(lifeFolderX)),
-        Integer.parseInt(myMagicValues.getString(simY))
-        , MouseButton.PRIMARY);
-    clickOn(Integer.parseInt(myMagicValues.getString(okX)),
-        Integer.parseInt(myMagicValues.getString(okY)), MouseButton.PRIMARY);
-    File actualFile = display.getMyFile();
-    try {
-      assertEquals(expectedFile.getCanonicalPath(), actualFile.getCanonicalPath());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
 
   /**
    * Unit Test that checks that after files are uploaded that a grid appears on the screen.
    */
   @Test
   public void checkGridIsNotEmpty() {
-    initialGridAction();
-    simulationTypeAction();
     clickOn(myStart);
     clickOn(myPause);
     assertTrue(assertGridViewExists());
@@ -153,8 +74,6 @@ public class CellSocietyViewTest extends DukeApplicationTest {
    */
   @Test
   public void checkGridInitialization() {
-    initialGridAction();
-    simulationTypeAction();
     clickOn(myStart);
     clickOn(myPause);
     assertTrue(checkGridMatches());
@@ -216,8 +135,6 @@ public class CellSocietyViewTest extends DukeApplicationTest {
    */
   @Test
   public void checkAnimationSpeedSlowedDown() {
-    initialGridAction();
-    simulationTypeAction();
     clickOn(myStart);
     double startSpeed = display.getAnimation().getRate();
     Button speedUp = lookup("SlowDown").query();
@@ -231,8 +148,6 @@ public class CellSocietyViewTest extends DukeApplicationTest {
    */
   @Test
   public void checkAnimationSpeedSpedUp() {
-    initialGridAction();
-    simulationTypeAction();
     clickOn(myStart);
     double startSpeed = display.getAnimation().getRate();
     Button speedUp = lookup("SpeedUp").query();
@@ -246,8 +161,6 @@ public class CellSocietyViewTest extends DukeApplicationTest {
    */
   @Test
   public void checkStepMethod() {
-    initialGridAction();
-    simulationTypeAction();
     clickOn(myStart);
     Button step = lookup("Step").query();
     clickOn(step);
