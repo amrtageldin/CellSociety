bcfb31bd56053302a7b83ac35034040418a8012f
Reya Magan <rm357@duke.edu>
2021-10-29 23:26:59 -0400

    wator is done!! will refactor next

--- src/cellsociety/model/WaTorModel.java
+++ src/cellsociety/model/WaTorModel.java
@@ -28,18 +28,18 @@ public class WaTorModel extends CellSocietyModel{
     public void setNextState(Cells myCell, int row, int col, Grid myGrid){
         stepCheck++;
         updateReproduction(myCell, 1, 0);
-        updateEnergy(myCell, -1, 5);
+        updateEnergy(myCell, -1, 2);
         int gridSize = myGrid.colLength() * myGrid.rowLength();
         if(stepCheck == gridSize + 1){
             changedCells.clear();
             stepCheck = 1;
         }
+        List<Cells> myNeighbors = generateNeighbors(row, col, myGrid);
+        checkEnergy(myCell);
+        updateNeighbors(myNeighbors);
+        checkReproduction(myCell, myNeighbors);
         if(!changedCells.contains(myCell)) {
-            List<Cells> myNeighbors = generateNeighbors(row, col, myGrid);
-            checkEnergy(myCell);
             int movableCells = findMovableCells(myCell, myNeighbors);
-            updateNeighbors(myNeighbors);
-            checkReproduction(myCell, myNeighbors);
             int state = getMyRules().generateNextState(movableCells, myCell.getCurrentState());
             checkState(myCell, state, myNeighbors);
         }
@@ -51,7 +51,9 @@ public class WaTorModel extends CellSocietyModel{
 
     private void updateReproduction(Cells myCell, int reproductionIncrease, int reproductionInitial){
         if(myCell.getCurrentState() == Integer.parseInt(getStatesBundle().getString(SHARK)) ||
-                myCell.getCurrentState() == Integer.parseInt(getStatesBundle().getString(FISH)) ) {
+                myCell.getCurrentState() == Integer.parseInt(getStatesBundle().getString(FISH)) ||
+                myCell.getMyNextState() == Integer.parseInt(getStatesBundle().getString(SHARK)) ||
+                myCell.getMyNextState() == Integer.parseInt(getStatesBundle().getString(FISH))) {
             if (!reproductionMap.containsKey(myCell)) {
                 reproductionMap.put(myCell, reproductionInitial);
             } else {
@@ -71,14 +73,19 @@ public class WaTorModel extends CellSocietyModel{
     }
     private void checkReproduction(Cells myCell, List<Cells> myNeighbors){
         if(reproductionMap.containsKey(myCell)) {
-            if (reproductionMap.get(myCell) == 5) {
+            if (reproductionMap.get(myCell) >= 5) {
                 Cells c = findRightState(myCell, myNeighbors, Integer.parseInt(getStatesBundle().getString(EMPTY)));
-                moveCells(c, myCell);
+                reproduceCells(c, myCell);
                 reproductionMap.remove(myCell);
                 updateReproduction(c, 1, 0);
                 updateReproduction(myCell, 1, 0);
             }
         }
+    }
+
+    private void reproduceCells(Cells c, Cells myCell){
+        c.setMyNextState(myCell.getCurrentState());
+        c.updateMyCurrentState();
     }
 
     private void checkEnergy(Cells myCell){
