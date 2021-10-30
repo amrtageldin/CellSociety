e7a4d21d68e75587cc78b28f2f5c53ac8f77133b
Reya Magan <rm357@duke.edu>
2021-10-30 17:34:44 -0400

    make public constant private

--- src/cellsociety/model/Cells.java
+++ src/cellsociety/model/Cells.java
@@ -2,7 +2,7 @@ package cellsociety.model;
 
 
 public class Cells {
-  public static final int INITIAL_NEXT_STATE = 10;
+  private static final int INITIAL_NEXT_STATE = 10;
   private int currentState;
   private Integer myNextState;
 
