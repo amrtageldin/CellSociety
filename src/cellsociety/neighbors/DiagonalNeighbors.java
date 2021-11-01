2d9bb7fcb1b1ab6834bac9f0d7555da0e4d98a8f
Reya Magan <rm357@duke.edu>
2021-11-01 01:57:16 -0400

    added diagonal neighbors

--- src/cellsociety/neighbors/DiagonalNeighbors.java
+++ src/cellsociety/neighbors/DiagonalNeighbors.java
@@ -6,26 +6,23 @@ import cellsociety.model.Cells;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RectangleNeighbors extends CellSocietyNeighbors{
-
-    public RectangleNeighbors(){super();}
+public class DiagonalNeighbors extends CellSocietyNeighbors{
+    public DiagonalNeighbors(){super();}
 
     public List<Cells> generateNeighbors(int row, int col, Grid myGrid) {
         int[] xChanges = new int[]{-1,1};
+        int[] yChanges = new int[]{-1,1};
         List<Cells> myCells = new ArrayList<>();
         for (int i : xChanges){
             if (rowIsValid(row + i, myGrid)){
-                myCells.add(myGrid.getCell(row+i, col));
+                for (int j : yChanges){
+                    if (colIsValid(col + j, myGrid)){
+                        myCells.add(myGrid.getCell(row + i, col + j));
+                    }
+                }
+
             }
         }
-
-        int[] yChanges = new int[]{-1,1};
-        for (int j : yChanges){
-            if (colIsValid(col + j, myGrid)){
-                myCells.add(myGrid.getCell(row,col+j));
-            }
-        }
-
         return myCells;
     }
 }
