31fdea529fa1a5fc6b50ce3e93030fbdddbfb4e6
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-11-01 02:21:46 -0400

    nit

--- src/cellsociety/view/CellSocietyViewComponents.java
+++ src/cellsociety/view/CellSocietyViewComponents.java
@@ -1,7 +1,6 @@
 package cellsociety.view;
 
 import cellsociety.controller.CellSocietyController;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
