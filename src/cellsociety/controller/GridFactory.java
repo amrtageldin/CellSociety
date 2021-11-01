a23fec62ec5d958b08c24075f4742c6941f8bd4c
Reya Magan <rm357@duke.edu>
2021-11-01 15:51:54 -0400

    added random cell state generation to configuration

--- src/cellsociety/controller/GridFactory.java
+++ src/cellsociety/controller/GridFactory.java
@@ -6,6 +6,7 @@ import com.opencsv.CSVReader;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Map;
 
 public class GridFactory {
     private static final String INVALID_GRID = "InvalidGrid";
@@ -13,7 +14,14 @@ public class GridFactory {
     private Grid myGrid;
     private String rowCount;
     private String colCount;
+    private String myValueSet;
+    private Map<String, String> gameStates;
 
+    public GridFactory(){
+        gameStates = Map.of("wa_tor", "0,1,2",
+                "Fire_Spread", "0,1,2", "game_of_life", "0,1", "percolation",
+                "0,1,2", "schelling_segregation", "0,2,3");
+    }
     /**
      * This method initially sets up the grid that has a state value for each cell. It reads in
      * a .csv file to find the number of rows and columns in the grid, and what the initial state
@@ -24,6 +32,7 @@ public class GridFactory {
      */
     public Grid setUpGrid(String file){
         try {
+            checkGameType(file);
             FileReader fileReader = new FileReader(file);
             CSVReader csvReader = new CSVReader(fileReader);
             initializeRowAndColumnCounts(csvReader);
@@ -37,6 +46,13 @@ public class GridFactory {
         return myGrid;
     }
 
+    private void checkGameType(String file){
+        for(String k : gameStates.keySet()){
+            if(file.contains(k)){
+                myValueSet = gameStates.get(k);
+            }
+        }
+    }
 
     private void initializeRowAndColumnCounts(CSVReader csvReader){
         try {
@@ -51,6 +67,7 @@ public class GridFactory {
 
     private void initializeGrid() {
         myGrid = new Grid(Integer.parseInt(rowCount),Integer.parseInt(colCount));
+        myGrid.getCellStates(myValueSet);
     }
 
     public ErrorFactory getMyErrorFactory(){ return myErrorFactory;}
