17b04f20afefffb735c57f7e86009e94978fd6bb
Amr Tagel-Din <aht14@duke.edu>
2021-11-01 23:46:51 -0400

    JavaDoc in Rule

--- src/cellsociety/rule/RuleEqual.java
+++ src/cellsociety/rule/RuleEqual.java
@@ -1,5 +1,9 @@
 package cellsociety.rule;
 
+/**
+ * The general rule class that will be used for equality comparisons; if we have a number
+ * that needs to be exactly equal
+ */
 public class RuleEqual extends Rule{
 
   public RuleEqual(int comparator, Integer conditionResult){
